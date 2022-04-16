1445f5e43051651dabac7101a7a3a8da932ffbeb
vtbossss <100446002+vtbossss@users.noreply.github.com>
2022-04-16 22:26:42 +0530

    Add files via upload

---
+++ farenhitescaletocelius.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"type the farenhite value"<<endl;
+    float n;
+    cin>>n;
+    float c=(n-32)*5/9;
+    cout<<"tempreture in degree celcius is "<<c;
+    }
